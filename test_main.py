8b4d16d7e4a5d48134472f6d23497d6fb518569a
jina <jina.lee.secu@gmail.com>
2022-03-17 22:16:45 +0900

    initial commit

---
+++ test_main.py
@@ -0,0 +1,19 @@
+x: int = 0
+a: int = []
+while (x != 0):
+    a.append(x%10)
+    x = x // 10
+
+print(x)
+print(a)    
+'''        
+b = int((''.join(map(str, a))))
+print(x)
+print(b)
+if x == b:
+    print (True)
+else:
+    print (False)
+        
+    '''
+    
