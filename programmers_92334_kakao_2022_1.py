441ac3dcc109097b8467472b90396b231dc2c5b8
jina <jina.lee.secu@gmail.com>
2022-03-24 10:53:31 +0900

    update_programmers_92334

--- programmers_92334_kakao_2022_1.py
+++ programmers_92334_kakao_2022_1.py
@@ -1,5 +1,52 @@
 def solution(id_list, report, k):
+    # 1. split reports by space ('reporter reportee')
+    # 2. create 'reportee_set' containing sets of 'reporter's 
+    
+    ## 
+    # 3. for reporter_set in reportee_set
+    # 4.    if len(reporter_set) < k  --> delete!
+    
+    ##
+    # id_list_dic = dictionary(id_list)
+    # 5. for reporter_set in reportee_set.value
+    # 6.    for reporter in reporter_set
+    # 7.        id_list_dic[repoter] += 1
+    # 8. return list(id_list_dic.values())
+    dictionary = dict.fromkeys(id_list) 
+    evr = []
     answer = []
+    count = 0
+
+
+    for i in id_list:
+        all_set = set()
+        for j in report:
+            evr = j.split()
+            if i == evr[1]:
+                a = dictionary[i]
+                if a == None:
+                    all_set.add(evr[0])
+                else:
+                    all_set = a
+                    all_set.add(evr[0])
+                dictionary[i] = all_set
+
+    for key, value in dictionary.items():
+        if value is not None:
+            if len(value) < k:
+                dictionary[key] = None
+
+    for i in id_list:
+        sum = 0
+        for key, value in dictionary.items():
+            value = dictionary[key]
+            if value is not None:
+                value = list(value)
+                if i in value:
+                    sum += 1
+        answer.insert(count,sum)
+        count += 1      
+
     return answer
 
 def test(id_list, report, k, result):
--- test_main.py
+++ test_main.py
@@ -1,19 +1,50 @@
-x: int = 0
-a: int = []
-while (x != 0):
-    a.append(x%10)
-    x = x // 10
+id_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
 
-print(x)
-print(a)    
-'''        
-b = int((''.join(map(str, a))))
-print(x)
-print(b)
-if x == b:
-    print (True)
-else:
-    print (False)
-        
-    '''
-    
+
+dictionary = dict.fromkeys(id_list) 
+evr = []
+result = []
+count = 0
+k = 2
+
+
+for i in id_list:
+    all_set = set()
+    for j in report:
+        evr = j.split()
+        if i == evr[1]:
+            a = dictionary[i]
+            if a == None:
+                all_set.add(evr[0])
+            else:
+                all_set = a
+                all_set.add(evr[0])
+            dictionary[i] = all_set
+
+for key, value in dictionary.items():
+    if value is not None:
+        if len(value) < k:
+            dictionary[key] = None
+
+for i in id_list:
+    sum = 0
+    for key, value in dictionary.items():
+        value = dictionary[key]
+        if value is not None:
+            value = list(value)
+            if i in value:
+                sum += 1
+    result.insert(count,sum)
+    count += 1      
+
+print(result)
+
+
+# for i in id_list:
+#     value = dictionary[i]
+#     sum = 0
+#     if value is not None:
+#         value = list(value)
+
+
