254125c28fc2b0ea3f9f4e40eefa39698d969293
jina <jina.lee.secu@gmail.com>
2022-04-11 11:52:01 +0900

    (new)programmers_81302

--- programmers_81301_2021kakao_internship01.py
+++ programmers_81301_2021kakao_internship01.py
@@ -6,21 +6,34 @@
 #           input dic(key) to the answer list 
 # 3. check s[] is digit
 #        input s to the answer list 
-        
-s = 'one4seveneight9eight'
-word_index = []
-result = []
-check = 0
+def solution(s):
+    dic_match = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
 
-#dic_match = {0:zero, 1:one, 2:two, 3:three, 4:four, 5:five, 6:six, 7:seven, 8:eight, 9:nine}
-dic_match = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+    result = ""
+    word = ""    
+    for value in s:
+        if value.isnumeric():
+            result += value
+            continue
+        # alphabet
+        word += value
+        if word in dic_match:
+            result += str(dic_match[word])
+            word = ""
+    return int(result)
+'''    
+    word_index = []
+    result = []
+    check = 0
 
 
-for num, i in enumerate(s):
-    if (i.isalpha()) == True:
-        word_index.append(i)
-    else:
-        while (len(word_index) != 0):
+
+    # PEP8
+    for num, i in enumerate(s):
+        if i.isalpha():
+            word_index.append(i)
+            continue
+        while len(word_index) != 0:
             serch = ''.join(s for s in word_index[0:2])
             for key, val in dic_match.items():
                 if serch in key:
@@ -30,12 +43,17 @@ for num, i in enumerate(s):
         result.append(i)
 
 
-while (len(word_index) != 0):
-            serch = ''.join(s for s in word_index[0:2])
-            for key, val in dic_match.items():
-                if serch in key:
-                    del word_index[0:len(key)] 
-                    result.append(val)
+    while (len(word_index) != 0):
+                serch = ''.join(s for s in word_index[0:2])
+                for key, val in dic_match.items():
+                    if serch in key:
+                        del word_index[0:len(key)] 
+                        result.append(val)
 
 
-answer = int(''.join(map(str, result)))
+    answer = int(''.join(map(str, result)))
+
+    return answer       
+'''
+
+print(solution('one4seveneight9eight'))
