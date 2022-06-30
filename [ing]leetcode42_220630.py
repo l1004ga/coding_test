903949724a811e47bb89e11ade9a5831cbfaa12d
jina <l1004ga@gmail.com>
2022-06-30 19:42:13 +0900

    220630_1

--- [ing]leetcode42_220630.py
+++ [ing]leetcode42_220630.py
@@ -1,9 +1,20 @@
-# 1. 가장 큰 수를 찾는다.
-
 def trap(height) -> int:
-    top = max(height)
-    height.index(top)
+    
+    volume = 0
+    left, right = 0, len(height)-1
+    left_max, right_max = height[left],height[right]
 
+    while left < right:
+        left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+
+        if left_max <= right_max:
+            volume += left_max - height[left]
+            left += 1
+        else: 
+            volume += right_max - height[right]
+            right -= 1
+    print(volume)
+    return volume
 
 
 
