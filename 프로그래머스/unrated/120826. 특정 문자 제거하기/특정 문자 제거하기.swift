bcc493207ae5ee18a0f20ebf826f9579f8564e4f
Jina <55937627+l1004ga@users.noreply.github.com>
2022-10-07 13:16:26 +0900

    [unrated] Title: 특정 문자 제거하기, Time: 0.08 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.swift
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.swift
@@ -2,4 +2,5 @@ import Foundation
 
 func solution(_ my_string:String, _ letter:String) -> String {
     return my_string.components(separatedBy: letter).joined()
+    return my_string.filter{String($0) != letter}
 }
