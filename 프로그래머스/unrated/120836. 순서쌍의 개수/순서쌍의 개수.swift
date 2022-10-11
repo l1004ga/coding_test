7912cce4326569b78c6122f97d9d519c207fef7f
Jina <55937627+l1004ga@users.noreply.github.com>
2022-10-11 13:41:44 +0900

    [unrated] Title: 순서쌍의 개수, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    
+    var result : Int = 0
+    
+    for i in 1...n {
+        if n % i == 0 {
+            result += 1
+        }
+    }
+    
+    return result
+}
