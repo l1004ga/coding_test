718b2929384b0cf99cbd9153a6b274c97efa3ea4
Jina <55937627+l1004ga@users.noreply.github.com>
2023-04-27 16:37:46 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ clothes:[[String]]) -> Int {
+    var typeCount : [String:Int] = [:]
+    
+    for i in clothes {
+        typeCount[i[1], default : 1] += 1
+    }
+    
+    return typeCount.reduce(1, { $0 * $1.value }) - 1
+}
