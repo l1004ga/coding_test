4873bc69b422315b81a87a0c4e22e744e3af8a97
Jina <55937627+l1004ga@users.noreply.github.com>
2023-06-27 19:16:32 +0900

    [level 2] Title: 전화번호 목록, Time: 209.15 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,60 @@
-def solution(phone_book):
-    items = set()
-    for phone in phone_book:
-        items.add(phone)
-    for phone in phone_book:
-        tmp = ""
-        for char in phone:
-            tmp += char
-            if tmp == phone:
-                break
-            if tmp in items:
-                return False
-    return True
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = {}
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+            node.word = False
+        node.word = True
+    
+    # 단어가 전부 존재하는지 여부
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return True
+            node = node.children[char]
+        return node.word
+    '''
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False 
+            node = node.children[char]
+        return True
+    '''
+    
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return True 
+            node = node.children[char]
+        if node.word == True:
+            return True
+        else:
+            return False   
+
+def solution(phoneBook):
+    phoneBook = sorted(phoneBook, key=len)
+    answer = True
+    t = Trie()
+    length = len(phoneBook)
+    
+    for phone in phoneBook:
+        t.insert(phone)
+        
+    for phone in phoneBook:
+        if not t.startsWith(phone):
+            answer = False
+    return answer
