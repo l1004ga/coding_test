8ec497f77a8b7fa49e3f963179084803dd4be472
Jina <55937627+l1004ga@users.noreply.github.com>
2023-09-08 17:12:45 +0900

    [level 2] Title: 타겟 넘버, Time: 2.73 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,44 +1,10 @@
-count = 0
-TARGET = 0
-
-class Node:
-    def __init__(self, item, numbers, breakpoint, depth=0):
-        self.item = item
-        self.left = None
-        self.right = None
-        self.depth = depth
-        self.numbers = numbers
-        self.breakpoint = breakpoint
-        
-    def createMySelf(self):
-        global count
-        if self.depth >= self.breakpoint:
-            global TARGET
-            if TARGET == self.item:
-                count += 1
-                # print(f'I am item: {self.item}, depth: {self.depth}, self.breakpoint: {self.breakpoint}, self.')
-            return
-        self.left = Node(self.item - self.numbers[self.depth], self.numbers, self.breakpoint, self.depth + 1)
-        self.right = Node(self.item + self.numbers[self.depth], self.numbers, self.breakpoint, self.depth + 1)
-        self.left.createMySelf()
-        self.right.createMySelf()
-
-class BinaryTree():
-    def __init__(self):
-        self.root = None
-
+def solve(numbers, depth, target, result, breakpoint):
+    if depth == breakpoint:
+        if result == target:
+            return 1
+        return 0
+    return solve(numbers, depth+1, target, result - numbers[depth], breakpoint) + solve(numbers, depth+1, target, result + numbers[depth], breakpoint)
+    
+    
 def solution(numbers, target):
-    # calc_tree = BinaryTree()
-    global TARGET
-    global count
-    count = 0
-    TARGET = target
-    rootNode = Node(0, numbers, len(numbers), 0)
-    rootNode.createMySelf()
-    # calc_tree.root = Node(0)
-    # flagNode = calc_tree.root
-    # for num in numbers:
-    #     flagNode.left = -num
-    #     flagNode.right = num
-    # print(count)
-    return count
+    return solve(numbers, 0, target, 0, len(numbers))
