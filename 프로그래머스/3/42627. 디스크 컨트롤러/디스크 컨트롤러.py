89114ababf4d934efc7dbfcfa9ab8df629a663f4
Jina <55937627+l1004ga@users.noreply.github.com>
2023-10-15 19:38:02 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,23 +1,29 @@
 import heapq
-from heapq import heappop, heappush
+from heapq import heappop, heappush, heapify
+
 
 def solution(jobs):
 
-    runtimes = 0
-    result = 0
+    현재시간 = 0
+    누적작업수행시간 = 0
+    
+    count = len(jobs)
     
     while jobs:
         my_heapq = []
         # now를 기점으로 이미 들어온 작업 중 수행시간이 가장 작은 작업 수행함
         for j in jobs:
-            if j[0] <= runtimes:
-                heapq.heappush(my_heapq, [runtimes + j[1] - j[0], j])
+            if j[0] <= 현재시간:
+                heapq.heappush(my_heapq, [j[1], j])
         
-              
+        if len(my_heapq) == 0:
+            현재시간 += 1
+            continue
+             
         # 가장 시간 작은 작업 수행 및 jobs 삭제 + 수행시간 업데이트
-        job = my_heapq[0][1]
-        runtimes += job[1]
-        result += my_heapq[0][0]
+        job = heappop(my_heapq)[1]
+        현재시간 += job[1]
+        누적작업수행시간 += 현재시간 - job[0]
         jobs.remove(job)
         
-    return result / 3
+    return int(누적작업수행시간 / count)
