c90645678034e6b7646195ec85261e9b2ed1afff
Jina <55937627+l1004ga@users.noreply.github.com>
2024-03-04 10:51:20 +0900

    Time: 28 ms (91.61%), Space: 16.4 MB (92.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,14 @@
+import collections
+
 class Solution:
+    dp = collections.defaultdict(int)
     def climbStairs(self, n: int) -> int:
-        dp = {}
 
         if n <= 2:
             return n
         
-        dp[1] = 1
-        dp[2] = 2
-
-        for i in range(3,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-
-        return dp[n]
+        if self.dp[n]:
+            return self.dp[n]
+        
+        self.dp[n] = self.climbStairs(n=n-1) + self.climbStairs(n=n-2)
+        return self.dp[n]
