54c5d8cdd8f3643f8b31cb52b262570b01ab93da
Jina <55937627+l1004ga@users.noreply.github.com>
2024-06-30 16:27:43 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -3,11 +3,11 @@ from collections import defaultdict, deque
 num_computer = int(input())
 connected_computer = int(input())
 
-connect_dict = defaultdict(dict)
+connect_dict = defaultdict(list)
 for _ in range(connected_computer):
     key, value = list(map(int, input().split()))
-    connect_dict[key][value] = True
-    connect_dict[value][key] = True
+    connect_dict[key].append(value)
+    connect_dict[value].append(key)
 
 visited = set()
 will_visit = deque()
@@ -16,8 +16,8 @@ while will_visit:
     now_node = will_visit.popleft()
     visited.add(now_node)
 
-    for key in connect_dict[now_node].keys():
-        if key not in visited:
-            will_visit.append(key)
+    for node in connect_dict[now_node]:
+        if node not in visited:
+            will_visit.append(node)
 
 print(len(visited) - 1)
